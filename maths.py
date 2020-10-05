95cc4359d05be3e0fa8d90e8f07df72895200e81
amine372 <nakhila@orange.fr>
2020-10-05 20:22:01 +0200

    Create maths.py

---
+++ maths.py
@@ -0,0 +1,11 @@
+import random
+import math
+clef = 6264522023
+texte = input(str("entre un texte"))
+Lettres = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+Tableau_Reference = []
+Tableau_pondere =[]
+for i in range (0,25):
+    Tableau_Reference.append(random.randint(1,99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999))
+
+k=2x+b
