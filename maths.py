7e6fea42979d2c3b540bc6143591a0e3e9bad9f2
amine372 <nakhila@orange.fr>
2020-10-05 20:38:46 +0200

    Update maths.py

--- maths.py
+++ maths.py
@@ -4,12 +4,11 @@ import binascii
 clef = 6264522023
 texte = input(str("entre un texte"))
 texte= list(texte.strip())
-texte1= []
 Lettres = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 Tableau_Reference = []
 Tableau_pondere =[]
 for i in range (0,25):
     Tableau_Reference.append(random.randint(1,99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999))
-for i in range(0,25):
-    texte1[i] = bin(int(binascii.hexlify(texte[i]), 16))[2:]
-print(texte1)
+
+u=binascii.b2a_hex(b'\xb9\x01\xef')
+print(u)
