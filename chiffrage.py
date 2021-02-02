5d9159905b54d768d31b7a1cbfb1d298a22cf3a6
amine372 <nakhila@orange.fr>
2021-02-02 20:06:05 +0100

    Update chiffrage.py

--- chiffrage.py
+++ chiffrage.py
@@ -3,10 +3,12 @@ import binascii
 texte = input(str("entre un texte"))
 
 liste_texte = list(texte.strip())
-print(liste_texte)
-for i in range(0,texte):
+
+for i in range(0,len(liste_texte)):
+    wh = liste_texte[i]
+    wh = bin(ord(texte))[2:]
 
 
 
 
-texte = bin(ord(texte))[2:]
+wh = bin(ord(texte))[2:]
